5b7a5dadd43da3f5ba018886945dadc84bef4061
Level126Noob <38739330+Level126Noob@users.noreply.github.com>
2019-08-03 20:40:24 -0400

    added server.js, moved html and css into 'public' folder

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
