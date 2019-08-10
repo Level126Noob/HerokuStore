27af95450ac80657a2d1093742c8aa9309acbf1d
Level126Noob <38739330+Level126Noob@users.noreply.github.com>
2019-08-10 12:44:27 -0400

    added jaws DB

--- server.js
+++ server.js
@@ -20,13 +20,16 @@ app.set("view engine", "handlebars");
 
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "bamazon"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'bamazon'
+  })
+}
 
 connection.connect(function (err) {
   if (err) {
