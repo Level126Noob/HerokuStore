4a043d722c14b6d257957a85a8a31a7d206ff8de
Level126Noob <38739330+Level126Noob@users.noreply.github.com>
2019-08-09 20:45:18 -0400

    got rid of other bamazon, made app.js instead, added all requests to server.js, created products.handlebars

---
+++ public/assets/js/app.js
@@ -0,0 +1,63 @@
+$(".deletebutton").on("click", (event) => {
+    let id = $(this).data("id");
+
+    $.ajax("/api/products/" + id, {
+        type: "DELETE"
+    }).then(
+        () => {
+            console.log("deleted id", id);
+            location.reload();
+        }
+    )
+})
+
+
+
+
+$(".addproduct").on("submit", function(event) {
+  // Make sure to preventDefault on a submit event.
+  event.preventDefault();
+
+  var newProduct = {
+    product_name: $("#product_name").val().trim(),
+    stock_quantity: $("#stock_quantity").val().trim(),
+    price: $("#price").val().trim()
+  };
+
+  // Send the POST request.
+  $.ajax("/api/products", {
+    type: "POST",
+    data: newProduct
+  }).then(
+    function() {
+      console.log(newProduct);
+    }
+  );
+});
+
+$(".update-form").on("submit", function(event) {
+  // Make sure to preventDefault on a submit event.
+  event.preventDefault();
+
+  var updatedProduct = {
+    product_name: $("#name").val().trim(),
+    stock_quantity: $("#quantity").val().trim(),
+    price: $("#pricez").val().trim()
+  };
+
+  var id = $(this).data("id");
+
+  // Send the POST request.
+  $.ajax("/api/products/" + id, {
+    type: "PUT",
+    data: updatedProduct
+  }).then(
+    function() {
+      console.log("updated product");
+      // Reload the page to get the updated list
+      location.assign("/");
+    }
+  );
+});
+
+
--- server.js
+++ server.js
@@ -1,26 +1,27 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-// Parse request body as JSON
+app.use(express.static(__dirname + "/public"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs ({ defaultLayout : "main" }))
 app.set("view engine", "handlebars");
 
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "bamazon"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon"
 });
 
 connection.connect(function(err) {
@@ -28,63 +29,77 @@ connection.connect(function(err) {
       console.error("error connecting: " + err.stack);
       return;
     }
-  
     console.log("connected as id " + connection.threadId);
   });
 
-  
-  
-  
-  app.get("/", (req, res) => {
-    connection.query("SELECT * FROM products;", function(err, data) {
+  errorFunc = (err, data) => {
       if (err) {
-        return res.status(500).end();
+          return res.status(500).send("It's broken dude");
+      }
+  }
+
+  app.get("/", (_, res) => {
+    connection.query("SELECT * FROM products", function(err, data) {
+        if (err) {
+            return res.status(500).send("it's broken dude");
+        }
+
+        res.render("index", { products: data });
+    });
+  });
+
+  app.get("/:id", function(req, res) {
+    connection.query("SELECT * FROM products where id = ?", [req.params.id], function(err, data) {
+      if (err) {
+        return res.status(500).send("it's broken dude");
       }
   
-      res.render("index", { products: data });
+      console.log(data);
+      res.render("products", data[0]);
     });
   });
   
-  // Create a new product
-  app.post("/api/products", (req, res) => {
-    connection.query("INSERT INTO products (product) VALUES (?)", [req.body.product], function(err, result) {
+
+  app.post("/api/products", function(req, res) {
+    connection.query("INSERT INTO products (product_name, stock_quantity, price) VALUES (?, ?, ?)", [req.body.product_name, req.body.stock_quantity, req.body.price], function(err, result) {
       if (err) {
-        return res.status(500).end();
+        return res.status(500).send("it's broken dude");
       }
+
+      res.json({ id: result.insertId});
+    })
+  })
   
-      res.json({ id: result.insertId });
-      console.log({ id: result.insertId });
-    });
-  });
   
-  // Update a product
-  app.put("/api/products/:id", (req, res) => {
-    connection.query("UPDATE products SET product = ? WHERE id = ?", [req.body.product, req.params.id], function(err, result) {
-      if (err) {
-        return res.status(500).end();
-      }
-      else if (result.changedRows === 0) {
-        return res.status(404).end();
-      }
-      res.status(200).end();
-  
-    });
-  });
-  
+
   app.delete("/api/products/:id", (req, res) => {
-    connection.query("DELETE FROM products WHERE id = ?", [req.params.id], function(err, result) {
-      if (err) {
-        return res.status(500).end();
-      }
-      else if (result.affectedRows === 0) {
-        return res.status(404).end();
-      }
-      res.status(200).end();
-  
-    });
+      connect.query("DELETE FROM products WHERE id = ?", [req.params.id], function(err, result) {
+          if(err) {
+              return res.status(500).send("it's broken dude");
+          }
+          else if (!result.affectedRows) {
+              return res.status(404).send("404 dude");
+          }
+          res.status(200).end();
+      });
+  });
+
+  app.put("api/products/:id", function(req, res) {
+      connection.query("UPDATE products SET product_name = ?, stock_quantity = ?, price = ? WHERE id = ?",
+      [req.body.product_name, req.body.stock_quantity, req.body.price,  req.body.id],
+      function(err, result) {
+          if (err) {
+              return res.status(500).send("it's broken dude");
+          }
+          else if (!result.changedRows) {
+              return res.status(404).send("404 dude");
+          }
+          res.status(200).end();
+      });
   });
   
+
   app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
+    console.log("The store is open on: http://localhost:" + PORT);
   });
   
